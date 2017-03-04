7480619cedb5969ae085edc24da8db7098754909
NODA, Kai <nodakai@gmail.com>
2017-03-04 20:38:05 +0800

    validate() can now call xmllint(1)

--- test/test.py
+++ test/test.py
@@ -4,10 +4,11 @@ import yaxml
 
 
 class Test(unittest.TestCase):
-    def test_is_file(self):
-        assert not yaxml.is_file('a')
-        assert not yaxml.is_file('/xyzzy')
-        assert yaxml.is_file('/etc/hosts')
-
-    def test1(self):
-        pass
+    def test_validate(self):
+        assert yaxml.validate(
+            '<foo/>',
+            '''
+<element name="foo" xmlns="http://relaxng.org/ns/structure/1.0" >
+    <empty/>
+</element>
+            ''')
--- yaxml/__init__.py
+++ yaxml/__init__.py
@@ -7,31 +7,46 @@ import yaml
 
 
 def is_file(s):
+    '''
+    >>> is_file('a')
+    False
+    >>> is_file('/xyzzy')
+    False
+    >>> is_file(__file__)
+    True
+    '''
+
     if 1 < len(s.split(os.linesep)):
         return False
     return os.path.exists(s)
 
 
 def validate(data, schema):
+    return 0 == run_validator(data, schema)[0]
+
+
+def run_validator(data, schema):
+
+    def with_temp_file(suf, data, fun):
+        with tempfile.NamedTemporaryFile(mode='w', suffix=".{}.xml".format(suf)) as fp:
+            path = fp.name
+            fp.write(data)
+            fp.flush()
+            return fun(path)
 
     def validate1(schema_path):
         def validate0(data_path):
-            p = subprocess.Popen(['xmllint', '--relaxng', schema_path, data_path])
+            p = subprocess.Popen(['xmllint', '--noout', '--relaxng', schema_path, data_path],
+                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             stdout, stderr = p.communicate()
             return (p.returncode, stdout, stderr)
 
         if is_file(data):
             return validate0(data)
         else:
-            with tempfile.NamedTemporaryFile(suffix='.xml') as data_file:
-                data_path = data_file.name
-                data_file.write(data)
-                return validate0(data_path)
+            return with_temp_file('data', data, validate0)
 
     if is_file(schema):
         return validate1(schema)
     else:
-        with tempfile.NamedTemporaryFile(suffix='.xml') as schema_file:
-            schema_path = schema_file.name
-            schema_file.write(schema)
-            return validate1(schema_path)
+        return with_temp_file('rng', schema, validate1)
