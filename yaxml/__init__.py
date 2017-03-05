dec76fb571a0841a7058b65a899341710ab93392
NODA, Kai <nodakai@gmail.com>
2017-03-06 01:35:02 +0800

    Support attributes
        schema:
            Elem:
                _attrA: true
                $attribute:
                    attrB: true

--- yaxml/__init__.py
+++ yaxml/__init__.py
@@ -38,41 +38,53 @@ MODIFIERS_D = dict(p for p in zip(['*', '+', '?'], MODIFIERS))
 
 
 class RngYamlParser(object):
-    def __init__(self, prefix):
-        self._prefix = prefix
+    def __init__(self, attr_prefix, directive_prefix):
+        self._attr_prefix = attr_prefix
+        self._directive_prefix = directive_prefix
         self._extra_checks = []
 
     def parse_pattern(self, path, y):  # I know it's very complex # noqa: C901
         if isinstance(y, dict):
+            interleave = []
             for k, v in y.items():
-                if self._prefix + 'attribute' == k:
-                    pass
-                elif self._prefix + 'interleave' == k:
-                    pass
-                elif self._prefix + 'choice' == k:
-                    pass
-                elif self._prefix + 'optional' == k:
-                    pass
-                elif self._prefix + 'list' == k:
-                    pass
-                elif self._prefix + 'mixed' == k:
-                    pass
-                elif self._prefix + 'ref' == k:
-                    pass
-                elif self._prefix + 'parentRef' == k:
-                    pass
-                elif self._prefix + 'value' == k:
-                    pass
-                elif self._prefix + 'data' == k:
-                    pass
-                elif self._prefix + 'notAllowed' == k:
-                    pass
-                elif self._prefix + 'externalRef' == k:
-                    pass
-                elif self._prefix + 'grammar' == k:
-                    pass
+                if k in (self._directive_prefix + 'attribute', self._directive_prefix + 'element'):
+                    element = k[len(self._directive_prefix): ]
+                    assert 1 == len(v), (
+                        "{}{} at {!r} MUST contain a sinle-element dict, but: {}".format(
+                            self._directive_prefix, element, path, pprint.pformat(v)))
+                    vk, vv = v.popitem()
+                    e = self.parse_element(path, vk, vv, element)
+                    interleave.append(e)
+                elif self._directive_prefix + 'interleave' == k:
+                    raise Exception("not supported")
+                elif self._directive_prefix + 'choice' == k:
+                    raise Exception("not supported")
+                elif self._directive_prefix + 'list' == k:
+                    raise Exception("not supported")
+                elif self._directive_prefix + 'mixed' == k:
+                    raise Exception("not supported")
+                elif self._directive_prefix + 'ref' == k:
+                    raise Exception("not supported")
+                elif self._directive_prefix + 'parentRef' == k:
+                    raise Exception("not supported")
+                elif self._directive_prefix + 'value' == k:
+                    raise Exception("not supported")
+                elif self._directive_prefix + 'data' == k:
+                    raise Exception("not supported")
+                elif self._directive_prefix + 'notAllowed' == k:
+                    raise Exception("not supported")
+                elif self._directive_prefix + 'externalRef' == k:
+                    raise Exception("not supported")
+                elif self._directive_prefix + 'grammar' == k:
+                    raise Exception("not supported")
                 else:
-                    return self.parse_element(path, k, v)
+                    e = self.parse_element(path, k, v, None)
+                    interleave.append(e)
+            if 1 == len(interleave):
+                return interleave[0]
+            if all('attribute' == e['element'] for e in interleave):
+                return interleave
+            return { 'element': 'interleave', 'child': interleave }
         elif isinstance(y, list):
             return [ self.parse_pattern(path, e) for e in y ]
         elif is_scalar(y):
@@ -80,15 +92,22 @@ class RngYamlParser(object):
         else:
             raise Exception("{}: what is {}".format(path, pprint.pformat(y)))
 
-    def parse_element(self, path, name, val):
+    def parse_element(self, path, name, val, element):
         mod = MODIFIERS_D.get(name[-1: ])
         if mod:
             realname = name[ :-1]
         else:
             realname = name
 
+        if not element:
+            if name.startswith(self._attr_prefix):
+                element = 'attribute'
+                realname = realname[len(self._attr_prefix): ]
+            else:
+                element = 'element'
+
         child = self.parse_pattern(path + '/' + realname, val)
-        ret = { 'element': 'element', 'name': realname, 'child': child }
+        ret = { 'name': realname, 'element': element, 'child': child }
 
         if mod:
             return { 'element': mod, 'child': ret }
@@ -102,6 +121,9 @@ class RngYamlParser(object):
             self._extra_checks.append((path, str(y)))
         return { 'element': 'text' }
 
+    def extra_checks(self):
+        return self._extra_checks
+
 
 def load_rngyaml(source, validate=True):
     source_abr = abbrev(source)
@@ -114,18 +136,16 @@ def load_rngyaml(source, validate=True):
         "RngYaml {!r} MUST have a mapping at the top-level, but: {}".format(
             source_abr, pprint.pformat(y))
 
-    prefix = y.get('prefix', '$')
+    attr_prefix = y.get('attr-prefix', '_')
+    directive_prefix = y.get('directive-prefix', '$')
     schema = y.get('schema')
-    assert isinstance(schema, dict) and 1 == len(schema), (
-        "RngYaml {!r} MUST have a //schema element which is a single mapping, but: {}".format(
-            source_abr, pprint.pformat(schema)))
-    parser = RngYamlParser(prefix)
+    parser = RngYamlParser(attr_prefix, directive_prefix)
     ret = parser.parse_pattern('/', schema)
 
     if validate:
         ret_as_xmlstr = ET.tostring(compile_rngyaml_to_rng(ret), 'unicode')
         rc, err = run_validator(ret_as_xmlstr, relaxng_in_relaxng.DATA)
-        assert 0 == rc, err
+        assert 0 == rc, err + ret_as_xmlstr
 
     return ret
 
@@ -137,17 +157,19 @@ def compile_rngyaml_to_rng(rngyaml):
     def parse(path, ry, xml):
         if isinstance(ry, dict):
             element = ry['element']
-            if 'element' == element:
+            if element in ('element', 'attribute'):
                 name = ry['name']
-                sub = ET.SubElement(xml, 'element')
+                sub = ET.SubElement(xml, element)
                 sub.set('name', name)
                 child = ry.get('child')
-                if None is not child:
-                    parse(path + '/' + 'element:' + name, child, sub)
+                parse(path + '/' + 'element:' + name, child, sub)
             elif element in MODIFIERS:
                 sub = ET.SubElement(xml, element)
                 child = ry['child']
                 parse(path + '/' + element, child, sub)
+            elif 'interleave' == element:
+                sub = ET.SubElement(xml, element)
+                parse(path + '/' + element, ry['child'], sub)
             elif 'text' == element:
                 sub = ET.SubElement(xml, element)
         elif isinstance(ry, list):
@@ -157,7 +179,11 @@ def compile_rngyaml_to_rng(rngyaml):
             raise Exception("{}: what is {}".format(path, pprint.pformat(ry)))
     dummy_root = ET.Element(None)
     parse('/', rngyaml, dummy_root)
-    root = next(child for child in dummy_root)
+    try:
+        root = next(child for child in dummy_root)
+    except Exception:
+        raise RngYamlParseError("empty result from " + pprint.pformat(rngyaml))
+
     root.set('xmlns', 'http://relaxng.org/ns/structure/1.0')
     return root
 
@@ -195,7 +221,7 @@ def load_yaml_as_xml(source, attribute_prefix='_'):
         "invalid YAML {!r}: the top-level mapping must contain a single entry "
         "(got {} entries)".format(source_abr, len(y)))
 
-    k, v = next(p for p in y.items())
+    k, v = y.popitem()
     root = ET.Element(str(k))
 
     def rec_add(path, yaml, xml):
@@ -267,7 +293,7 @@ def run_validator(data, schema=None):
             cmdline.append(data_path)
             p = subprocess.Popen(cmdline, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             stdout, stderr = p.communicate()
-            return (p.returncode, stderr)
+            return (p.returncode, stderr.decode('utf-8'))
 
         return with_prepared_input(data, 'data', validate0)
 
--- yaxml/test_0.py
+++ yaxml/test_0.py
@@ -1,7 +1,12 @@
-from copy import deepcopy as dcp
 import xml.etree.ElementTree as ET
+import xml.dom.minidom as dom
+from pprint import pprint as pp, pformat as pf
 
 import yaxml
+
+
+def xmlpp(xmlstr):
+    return dom.parseString(xmlstr).toprettyxml(indent="  ", newl="\n")
 
 
 def test_validate():
@@ -28,14 +33,19 @@ Root:
             xml.getroot(), 'unicode'))
 
 
-Y0 = yaxml.load_rngyaml('''
+SIMPLE = '''
 schema:
     addressBook: true
-''')
+'''
 
 
-def test_load_rngyaml_0():
-    y = dcp(Y0)
+def load_rngyaml(s):
+    # disable automatic validation
+    return yaxml.load_rngyaml(s, False)
+
+
+def test_load_rngyaml_simple():
+    y = load_rngyaml(SIMPLE)
 
     assert {
         'element': 'element',
@@ -46,16 +56,50 @@ def test_load_rngyaml_0():
     } == y
 
 
-Y1 = yaxml.load_rngyaml('''
+ELEM_SEQ_0 = '''
+schema:
+    addressBook:
+        - name: true
+        - name: true
+'''
+
+
+def test_load_rngyaml_elem_seq_0():
+    y = load_rngyaml(ELEM_SEQ_0)
+
+    y0 = y.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'addressBook'
+    } == y
+
+    y1, y2 = y0
+    assert {
+        'element': 'element',
+        'name': 'name',
+        'child': {
+            'element': 'text'
+        }
+    } == y1
+    assert {
+        'element': 'element',
+        'name': 'name',
+        'child': {
+            'element': 'text'
+        }
+    } == y2
+
+
+ELEM_SEQ_1 = '''
 schema:
     addressBook:
         - name: true
         - email: true
-''')
+'''
 
 
-def test_load_rngyaml_1():
-    y = dcp(Y1)
+def test_load_rngyaml_elem_seq_1():
+    y = load_rngyaml(ELEM_SEQ_1)
 
     y0 = y.pop('child')
     assert {
@@ -80,17 +124,16 @@ def test_load_rngyaml_1():
     } == y2
 
 
-Y2 = yaxml.load_rngyaml('''
+ELEM_DIC = '''
 schema:
     addressBook:
-        card*:
-            - name: true
-            - email: true
-''')
+        name: true
+        email: true
+'''
 
 
-def test_load_rngyaml_2():
-    y = dcp(Y2)
+def test_load_rngyaml_elem_dic():
+    y = load_rngyaml(ELEM_DIC)
 
     y0 = y.pop('child')
     assert {
@@ -100,37 +143,514 @@ def test_load_rngyaml_2():
 
     y1 = y0.pop('child')
     assert {
-        'element': 'zeroOrMore',
+        'element': 'interleave'
     } == y0
 
-    y2 = y1.pop('child')
-    assert {
-        'element': 'element',
-        'name': 'card'
-    } == y1
+    y2, y3 = y1
+    if y2['name'] != 'name':
+        y2, y3 = y3, y2
 
-    y3, y4 = y2
     assert {
         'element': 'element',
         'name': 'name',
         'child': {
             'element': 'text'
         }
-    } == y3
+    } == y2
+
     assert {
         'element': 'element',
         'name': 'email',
         'child': {
             'element': 'text'
         }
+    } == y3
+
+
+ZERO_OR_MORE = '''
+schema:
+    r:
+        a*:
+            p: true
+        b*:
+            q: true
+'''
+
+
+def test_load_rngyaml_zero_or_more():
+    y = load_rngyaml(ZERO_OR_MORE)
+
+    y0 = y.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'r'
+    } == y
+
+    y1 = y0.pop('child')
+    assert {
+        'element': 'interleave',
+    } == y0
+
+    y2, y3 = y1
+    if y2['child']['name'] == 'b':
+        y2, y3 = y3, y2
+
+    y20 = y2.pop('child')
+    assert {
+        'element': 'zeroOrMore',
+    } == y2
+
+    y21 = y20.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'a'
+    } == y20
+
+    assert {
+        'element': 'element',
+        'name': 'p',
+        'child': {
+            'element': 'text'
+        }
+    } == y21
+
+    y30 = y3.pop('child')
+    assert {
+        'element': 'zeroOrMore',
+    } == y3
+
+    y31 = y30.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'b'
+    } == y30
+
+    assert {
+        'element': 'element',
+        'name': 'q',
+        'child': {
+            'element': 'text'
+        }
+    } == y31
+
+
+ONE_ATTR_ONLY = '''
+schema:
+    rr:
+        _aa: true
+'''
+
+
+def test_load_rngyaml_one_attr_only():
+    y = load_rngyaml(ONE_ATTR_ONLY)
+
+    y0 = y.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'rr'
+    } == y
+
+    assert {
+        'element': 'attribute',
+        'name': 'aa',
+        'child': {
+            'element': 'text'
+        }
+    } == y0
+
+
+ATTR_ONLY = '''
+schema:
+    r0:
+        _a0: true
+        _b0: true
+'''
+
+
+def test_load_rngyaml_attr_only():
+    y = load_rngyaml(ATTR_ONLY)
+
+    y0 = y.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'r0'
+    } == y
+
+    y1, y2 = y0
+    if y1['name'] == 'b0':
+        y1, y2 = y2, y1
+
+    assert {
+        'element': 'attribute',
+        'name': 'a0',
+        'child': {
+            'element': 'text'
+        }
+    } == y1
+
+    assert {
+        'element': 'attribute',
+        'name': 'b0',
+        'child': {
+            'element': 'text'
+        }
+    } == y2
+
+
+ATTR_ONLY_ARR = '''
+schema:
+    r1:
+        - _a1: true
+        - _b1: true
+'''
+
+
+def test_load_rngyaml_attr_only_arr():
+    y = load_rngyaml(ATTR_ONLY_ARR)
+
+    y0 = y.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'r1'
+    } == y
+
+    y1, y2 = y0
+    if y1['name'] == 'b1':
+        y1, y2 = y2, y1
+
+    assert {
+        'element': 'attribute',
+        'name': 'a1',
+        'child': {
+            'element': 'text'
+        }
+    } == y1
+
+    assert {
+        'element': 'attribute',
+        'name': 'b1',
+        'child': {
+            'element': 'text'
+        }
+    } == y2
+
+
+ESCAPED_0 = '''
+schema:
+    r2:
+        $element:
+            e: true
+'''
+
+
+def test_load_rngyaml_escaped_0():
+    y = load_rngyaml(ESCAPED_0)
+
+    y0 = y.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'r2'
+    } == y
+
+    assert {
+        'element': 'element',
+        'name': 'e',
+        'child': {
+            'element': 'text'
+        }
+    } == y0
+
+
+ESCAPED_1 = '''
+schema:
+    r3:
+        - $element:
+            e3: true
+'''
+
+
+def test_load_rngyaml_escaped_1():
+    y = load_rngyaml(ESCAPED_1)
+
+    y0 = y.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'r3'
+    } == y
+
+    assert [{
+        'element': 'element',
+        'name': 'e3',
+        'child': {
+            'element': 'text'
+        }
+    }] == y0
+
+
+ESCAPED_2 = '''
+schema:
+    r4:
+        - $element:
+            e4: true
+        - $element:
+            e4: true
+'''
+
+
+def test_load_rngyaml_escaped_2():
+    y = load_rngyaml(ESCAPED_2)
+
+    y0 = y.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'r4'
+    } == y
+
+    y1, y2 = y0
+
+    assert {
+        'element': 'element',
+        'name': 'e4',
+        'child': {
+            'element': 'text'
+        }
+    } == y1
+
+    assert {
+        'element': 'element',
+        'name': 'e4',
+        'child': {
+            'element': 'text'
+        }
+    } == y2
+
+
+ESCAPED_ATTR_0 = '''
+schema:
+    r5:
+        $attribute:
+            a: true
+'''
+
+
+def test_load_rngyaml_escaped_attr_0():
+    y = load_rngyaml(ESCAPED_ATTR_0)
+
+    y0 = y.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'r5'
+    } == y
+
+    assert {
+        'element': 'attribute',
+        'name': 'a',
+        'child': {
+            'element': 'text'
+        }
+    } == y0
+
+
+ESCAPED_ATTR_1 = '''
+schema:
+    r6:
+        - $attribute:
+            a: true
+'''
+
+
+def test_load_rngyaml_escaped_attr_1():
+    y = load_rngyaml(ESCAPED_ATTR_1)
+
+    y0 = y.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'r6'
+    } == y
+
+    assert [{
+        'element': 'attribute',
+        'name': 'a',
+        'child': {
+            'element': 'text'
+        }
+    }] == y0
+
+
+ESCAPED_ATTR_2 = '''
+schema:
+    r7:
+        $attribute:
+            a: true
+        _b: true
+'''
+
+
+def test_load_rngyaml_escaped_attr_2():
+    y = load_rngyaml(ESCAPED_ATTR_2)
+
+    y0 = y.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'r7'
+    } == y
+
+    y1, y2 = y0
+    if y1['name'] == 'b':
+        y1, y2 = y2, y1
+
+    assert {
+        'element': 'attribute',
+        'name': 'a',
+        'child': {
+            'element': 'text'
+        }
+    } == y1
+
+    assert {
+        'element': 'attribute',
+        'name': 'b',
+        'child': {
+            'element': 'text'
+        }
+    } == y2
+
+
+ESCAPED_ATTR_3 = '''
+schema:
+    r8:
+        - card*:
+            _name: true
+            $attribute:
+                email: true
+        - $element:
+            e: true
+        - $element:
+            _e: true
+        - $element:
+            __e: true
+        - $element:
+            e:
+                e: true
+'''
+
+
+def test_load_rngyaml_escaped_attr_3():
+    y = load_rngyaml(ESCAPED_ATTR_3)
+
+    y0 = y.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'r8'
+    } == y
+
+    y1, y2, y3, y4, y5 = y0
+
+    y10 = y1.pop('child')
+    assert {
+        'element': 'zeroOrMore',
+    } == y1
+
+    y11 = y10.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'card'
+    } == y10
+
+    y12, y13 = y11
+    if y12['name'] == 'email':
+        y12, y13 = y13, y12
+
+    assert {
+        'element': 'attribute',
+        'name': 'name',
+        'child': {
+            'element': 'text'
+        }
+    } == y12
+
+    assert {
+        'element': 'attribute',
+        'name': 'email',
+        'child': {
+            'element': 'text'
+        }
+    } == y13
+
+    assert {
+        'element': 'element',
+        'name': 'e',
+        'child': {
+            'element': 'text'
+        }
+    } == y2
+
+    assert {
+        'element': 'element',
+        'name': '_e',
+        'child': {
+            'element': 'text'
+        }
+    } == y3
+
+    assert {
+        'element': 'element',
+        'name': '__e',
+        'child': {
+            'element': 'text'
+        }
     } == y4
+
+    assert {
+        'element': 'element',
+        'name': 'e',
+        'child': {
+            'element': 'element',
+            'name': 'e',
+            'child': {
+                'element': 'text'
+            }
+        }
+    } == y5
 
 
 def test_compile_rngyaml_to_rng():
     def f(y):
-        s = ET.tostring(yaxml.compile_rngyaml_to_rng(dcp(y)), 'unicode')
-        return yaxml.validate(s, yaxml.relaxng_in_relaxng.DATA)
+        internal = load_rngyaml(y)
+        xmlstr = ET.tostring(yaxml.compile_rngyaml_to_rng(internal), 'unicode')
+        pped = xmlpp(xmlstr)
+        rc, err = yaxml.run_validator(pped, yaxml.relaxng_in_relaxng.DATA)
+        return (0 == rc, err + '\n' + pf(internal) + '\n' + pped)
 
-    assert f(Y0)
-    assert f(Y1)
-    assert f(Y2)
+    ok, err = f(SIMPLE)
+    assert ok, err
+    ok, err = f(ELEM_SEQ_0)
+    assert ok, err
+    ok, err = f(ELEM_SEQ_1)
+    assert ok, err
+    ok, err = f(ELEM_DIC)
+    assert ok, err
+    ok, err = f(ZERO_OR_MORE)
+    assert ok, err
+    ok, err = f(ONE_ATTR_ONLY)
+    assert ok, err
+    ok, err = f(ATTR_ONLY)
+    assert ok, err
+    ok, err = f(ATTR_ONLY_ARR)
+    assert ok, err
+    ok, err = f(ESCAPED_0)
+    assert ok, err
+    ok, err = f(ESCAPED_1)
+    assert ok, err
+    ok, err = f(ESCAPED_2)
+    assert ok, err
+    ok, err = f(ESCAPED_ATTR_0)
+    assert ok, err
+    ok, err = f(ESCAPED_ATTR_1)
+    assert ok, err
+    ok, err = f(ESCAPED_ATTR_2)
+    assert ok, err
+    ok, err = f(ESCAPED_ATTR_3)
+    assert ok, err
+#   ok, err = f(ESCAPED_3)
+#   assert ok, err
