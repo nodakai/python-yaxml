3d55dc4a66adac895500706be646e3a07cff6b16
NODA, Kai <nodakai@gmail.com>
2017-03-05 17:06:38 +0800

    + RngYamlParser: can load simple RngYaml

--- yaxml/__init__.py
+++ yaxml/__init__.py
@@ -3,11 +3,127 @@ import os.path
 import tempfile
 import subprocess
 import numbers
+import pprint
 import xml.etree.ElementTree as ET
 
 import yaml as pyyaml
 
 from . import relaxng_in_relaxng
+
+
+def abbrev(source):
+    '''
+    >>> abbrev('01234567890123456789012')
+    '01234567890123456789012'
+    >>> abbrev('0123456789012345678901234')
+    '0123456789...5678901234'
+    '''
+
+    N = 10
+    return source if len(source) <= 2 * N + 3 else source[ :N] + '...' + source[-N: ]
+
+
+class Exc(Exception):
+    def __init__(self, fmt, *args):
+        super(Exc, self).__init__(fmt.format(*args))
+
+
+class RngYamlParseError(Exc):
+    def __init__(self, fmt, *args):
+        super(RngYamlParseError, self).__init__(fmt, *args)
+
+
+MODIFIERS = { '*': 'zeroOrMore', '+': 'oneOrMore', '?': 'optional' }
+
+
+class RngYamlParser(object):
+    def __init__(self, prefix):
+        self._prefix = prefix
+        self._extra_checks = []
+
+    def parse_pattern(self, path, y):
+        if isinstance(y, dict):
+            for k, v in y.items():
+                if self._prefix + 'attribute' == k:
+                    pass
+                elif self._prefix + 'interleave' == k:
+                    pass
+                elif self._prefix + 'choice' == k:
+                    pass
+                elif self._prefix + 'optional' == k:
+                    pass
+                elif self._prefix + 'list' == k:
+                    pass
+                elif self._prefix + 'mixed' == k:
+                    pass
+                elif self._prefix + 'ref' == k:
+                    pass
+                elif self._prefix + 'parentRef' == k:
+                    pass
+                elif self._prefix + 'empty' == k:
+                    pass
+                elif self._prefix + 'text' == k:
+                    pass
+                elif self._prefix + 'value' == k:
+                    pass
+                elif self._prefix + 'data' == k:
+                    pass
+                elif self._prefix + 'notAllowed' == k:
+                    pass
+                elif self._prefix + 'externalRef' == k:
+                    pass
+                elif self._prefix + 'grammar' == k:
+                    pass
+                else:
+                    return self.parse_element(path, k, v)
+        elif isinstance(y, list):
+            return [ self.parse_pattern(path, e) for e in y ]
+        elif is_scalar(y):
+            return self.parse_text(path, y)
+        else:
+            raise Exception("{}: what is {}".format(path, pprint.pformat(y)))
+
+    def parse_element(self, path, name, val):
+        mod = MODIFIERS.get(name[-1: ])
+        if mod:
+            realname = name[ :-1]
+        else:
+            realname = name
+
+        child = self.parse_pattern(path + '/' + realname, val)
+        ret = { 'element': 'element', 'name': realname, 'child': child }
+
+        if mod:
+            return { 'element': mod, 'child': ret }
+        else:
+            return ret
+
+    def parse_text(self, path, y):
+        if None is y or False is y:
+            return { 'element': 'empty' }
+        if True is not y:
+            self._extra_checks.append((path, str(y)))
+        return { 'element': 'text' }
+
+
+def load_rngyaml(source):
+    source_abr = abbrev(source)
+    if is_filepath(source):
+        with open(source) as fp:
+            y = pyyaml.load(fp)
+    else:
+        y = pyyaml.load(source)
+
+    assert isinstance(y, dict), \
+        "RngYaml {!r} MUST have a mapping at the top-level, but: {}".format(
+            source_abr, pprint.pformat(y))
+    prefix = y.get('prefix', '$')
+    schema = y.get('schema')
+    assert isinstance(schema, dict) and 1 == len(schema), (
+        "RngYaml {!r} MUST have a //schema element which is a single mapping, but: {}".format(
+            source_abr, pprint.pformat(schema)))
+    parser = RngYamlParser(prefix)
+    return parser.parse_pattern('/', schema)
 
 
 def is_scalar(x):
@@ -34,14 +150,14 @@ def is_scalar(x):
 
 
 def load_yaml_as_xml(source, attribute_prefix='_'):
-    source_human = source if len(source) <= 13 else source[ :5] + '...' + source[-5: ]
+    source_abr = source if len(source) <= 13 else source[ :5] + '...' + source[-5: ]
     y = pyyaml.load(source)
     assert isinstance(y, dict), \
         "invalid YAML {!r}: the top-level datatype must be a mapping (got {})".format(
-            source_human, type(y))
+            source_abr, type(y))
     assert 1 == len(y), (
         "invalid YAML {!r}: the top-level mapping must contain a single entry "
-        "(got {} entries)".format(source_human, len(y)))
+        "(got {} entries)".format(source_abr, len(y)))
 
     k, v = next(p for p in y.items())
     root = ET.Element(str(k))
@@ -66,23 +182,23 @@ def load_yaml_as_xml(source, attribute_prefix='_'):
     rec_add('/', v, root)
 
     assert validate(ET.tostring(root, 'unicode')), \
-        "YAML {!r} yields an invalid XML".format(source_human)
+        "YAML {!r} yields an invalid XML".format(source_abr)
 
     return ET.ElementTree(root)
 
 
-def is_file(s):
+def is_filepath(s):
     '''
-    >>> is_file('a')
+    >>> is_filepath('a')
     False
-    >>> is_file('/xyzzy')
+    >>> is_filepath('/xyzzy')
     False
-    >>> is_file(__file__)
+    >>> is_filepath(__file__)
     True
     '''
 
-    assert isinstance(s, str), type(s)
-    if 1 < len(s.split(os.linesep)):
+    assert isinstance(s, str), "invalid input {!r} of type {}".format(s, type(s))
+    if os.linesep in s:
         return False
     return os.path.exists(s)
 
@@ -100,6 +216,13 @@ def run_validator(data, schema=None):
             fp.flush()
             return fun(path)
 
+    def with_prepared_input(source, tmp_sfx, fun):
+        if is_filepath(source):
+            with open(source) as fp:
+                return fun(fp)
+        else:
+            return with_temp_file(tmp_sfx, source, fun)
+
     def validate1(schema_path):
         def validate0(data_path):
             cmdline = ['xmllint', '--noout']
@@ -110,12 +233,8 @@ def run_validator(data, schema=None):
             stdout, stderr = p.communicate()
             return (p.returncode, stdout, stderr)
 
-        if is_file(data):
-            return validate0(data)
-        else:
-            return with_temp_file('data', data, validate0)
+        return with_prepared_input(data, 'data', validate0)
 
-    if None is schema or is_file(schema):
+    if None is schema:
         return validate1(schema)
-    else:
-        return with_temp_file('rng', schema, validate1)
+    return with_prepared_input(schema, 'rng', validate1)
--- yaxml/test_0.py
+++ yaxml/test_0.py
@@ -1,28 +1,113 @@
-import unittest
 import xml.etree.ElementTree as ET
 
 import yaxml
 
 
-class Test(unittest.TestCase):
-    def test_validate(self):
-        assert yaxml.validate(
-            '<foo/>',
-            '''
+def test_validate():
+    assert yaxml.validate('<foo/>', '''
 <element name="foo" xmlns="http://relaxng.org/ns/structure/1.0" >
-    <empty/>
+<empty/>
 </element>
-            ''')
+        ''')
 
-        assert yaxml.validate(yaxml.relaxng_in_relaxng.DATA, yaxml.relaxng_in_relaxng.DATA)
+    assert yaxml.validate(yaxml.relaxng_in_relaxng.DATA,
+                          yaxml.relaxng_in_relaxng.DATA)
 
-    def test_load_yaml_as_xml(self):
-        xml = yaxml.load_yaml_as_xml('''
+
+def test_load_yaml_as_xml():
+    xml = yaxml.load_yaml_as_xml('''
 Root:
     Foo:
         Bar:
             _baz: 1
             _quux: true
-        ''')
-        assert ('<Root><Foo><Bar _baz="1" _quux="true" /></Foo></Root>' ==
-                ET.tostring(xml.getroot(), 'unicode'))
+    ''')
+    assert (
+        '<Root><Foo><Bar _baz="1" _quux="true" /></Foo></Root>' == ET.tostring(
+            xml.getroot(), 'unicode'))
+
+
+def test_load_rngyaml_0():
+    y = yaxml.load_rngyaml('''
+schema:
+    addressBook: true
+    ''')
+    assert {
+        'element': 'element',
+        'name': 'addressBook',
+        'child': {
+            'element': 'text',
+        }
+    } == y
+
+def test_load_rngyaml_1():
+    y = yaxml.load_rngyaml('''
+schema:
+    addressBook:
+        - name: true
+        - email: true
+    ''')
+
+    y0 = y.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'addressBook'
+    } == y
+
+    y1, y2 = y0
+    assert {
+        'element': 'element',
+        'name': 'name',
+        'child': {
+            'element': 'text'
+        }
+    } == y1
+    assert {
+        'element': 'element',
+        'name': 'email',
+        'child': {
+            'element': 'text'
+        }
+    } == y2
+
+def test_load_rngyaml_2():
+    y = yaxml.load_rngyaml('''
+schema:
+    addressBook:
+        card*:
+            - name: true
+            - email: true
+    ''')
+
+    y0 = y.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'addressBook'
+    } == y
+
+    y1 = y0.pop('child')
+    assert {
+        'element': 'zeroOrMore',
+    } == y0
+
+    y2 = y1.pop('child')
+    assert {
+        'element': 'element',
+        'name': 'card'
+    } == y1
+
+    y3, y4 = y2
+    assert {
+        'element': 'element',
+        'name': 'name',
+        'child': {
+            'element': 'text'
+        }
+    } == y3
+    assert {
+        'element': 'element',
+        'name': 'email',
+        'child': {
+            'element': 'text'
+        }
+    } == y4
